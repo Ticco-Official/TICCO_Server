05db15977d06a389f190d156b0f48de8a6df61d7
HyeokJoon Kong <orijoon98@gmail.com>
2022-08-01 16:26:06 +0900

    [DOCS] swagger 문서 업데이트

--- src/main/java/com/ticco/controller/auth/dto/request/LoginRequestDto.java
+++ src/main/java/com/ticco/controller/auth/dto/request/LoginRequestDto.java
@@ -2,6 +2,7 @@ package com.ticco.controller.auth.dto.request;
 
 import com.ticco.domain.user.UserSocialType;
 import com.ticco.service.auth.dto.request.LoginDto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -13,9 +14,11 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginRequestDto {
 
+    @ApiModelProperty(value = "소셜 로그인 타입 - KAKAO, APPLE", example = "KAKAO")
     @NotNull(message = "{user.socialType.notNull}")
     private UserSocialType socialType;
 
+    @ApiModelProperty(value = "토큰 - socialToken", example = "ijv4qLk0I7jYuDpFe-9A-oAx59-AAfC6UbTuairPCj1zTQAAAYI6e-6o")
     @NotBlank(message = "{auth.token.notBlank}")
     private String token;
 
--- src/main/java/com/ticco/service/auth/dto/request/TokenRequestDto.java
+++ src/main/java/com/ticco/service/auth/dto/request/TokenRequestDto.java
@@ -1,5 +1,6 @@
 package com.ticco.service.auth.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @ToString
@@ -8,6 +9,9 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TokenRequestDto {
 
+    @ApiModelProperty(value = "토큰 - accessToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJVU0VSX0lEIjoxLCJleHAiOjE2NTg4NDA2NzN9.udnKnDSK08EuX56E5k-vkYUbZYofuo12YdiM9gEPY4eqdfzM_xt4MpgTimTuQ8ipmMxWZNCaTjtentg8vLyfgQ")
     private String accessToken;
+
+    @ApiModelProperty(value = "토큰 - refreshToken", example = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2NTk0NDM2NzN9.1L4eWqLGvob8jsTe5ZQVbmWpitVjZ0wMIoYRg6qPyum1iLaVOV_AT6nM0FtO5OrMM_9VXRWzMaON2S4E_QsxzQ")
     private String refreshToken;
 }
