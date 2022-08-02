4eed5665a6b9b39fcee0ce9c3c035df439b2d919
HyeokJoon Kong <orijoon98@gmail.com>
2022-08-02 19:03:32 +0900

    [FIX] API method 변경

--- src/main/java/com/ticco/controller/user/UserController.java
+++ src/main/java/com/ticco/controller/user/UserController.java
@@ -8,7 +8,7 @@ import com.ticco.service.user.dto.request.UpdateOnboardingInfoRequestDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,9 +23,9 @@ public class UserController {
 
     private final UserService userService;
 
-    @ApiOperation("[인증] 나의 온보딩 정보를 설정합니다.")
+    @ApiOperation("[인증] 나의 온보딩 정보를 업데이트합니다.")
     @Auth
-    @PostMapping("/v1/user/onboarding")
+    @PutMapping("/v1/user/onboarding")
     public ApiResponse<String> updateOnboardingInfo(@Valid UpdateOnboardingInfoRequestDto request,
                                                     @RequestPart(required = false) MultipartFile image,
                                                     @ApiIgnore @UserId Long userId) {
