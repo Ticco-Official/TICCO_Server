d7e4dd9a177398e30cf3a4a2a35dcc76814e96d9
HyeokJoon Kong <orijoon98@gmail.com>
2022-08-02 20:22:59 +0900

    [DOCS] API 명세서 수정

--- src/main/java/com/ticco/service/user/dto/request/UpdateOnboardingInfoRequestDto.java
+++ src/main/java/com/ticco/service/user/dto/request/UpdateOnboardingInfoRequestDto.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UpdateOnboardingInfoRequestDto {
 
-    @ApiModelProperty(value = "닉네임", example = "혁돌몬")
+    @ApiModelProperty(value = "닉네임", example = "혁돌몬", required = true)
     @Length(min = 1, max = 6, message = "{onboarding.nickname.length}")
     @NotBlank(message = "{onboarding.nickname.notBlank}")
     private String nickname;
